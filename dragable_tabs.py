cbcdf82512b7255a44fbff5b4756afebf59d27a6
SlinkierSwine87 <khanipovm@gmail.com>
2020-11-03 19:42:30 +0500

    Secuted with dragable tabs v1

---
+++ dragable_tabs.py
@@ -0,0 +1,65 @@
+import sys
+
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+
+class DragableTabs(QTabWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+        self.setAcceptDrops(True)
+        self.tabBar = self.tabBar()
+        self.tabBar.setMouseTracking(True)
+        self.indexTab = None
+        self.setTabShape(QTabWidget.Triangular)
+        self.setMovable(True)
+        self.setDocumentMode(True)
+        self.setTabsClosable(True)
+
+    def mouseMoveEvent(self, e):
+        if e.buttons() != Qt.RightButton:
+            return
+
+        globalPos = self.mapToGlobal(e.pos())
+        tabBar = self.tabBar
+        posInTab = tabBar.mapFromGlobal(globalPos)
+        self.indexTab = tabBar.tabAt(e.pos())
+        tabRect = tabBar.tabRect(self.indexTab)
+
+        pixmap = QPixmap(tabRect.size())
+        tabBar.render(pixmap, QPoint(), QRegion(tabRect))
+        mimeData = QMimeData()
+        drag = QDrag(tabBar)
+        drag.setMimeData(mimeData)
+        drag.setPixmap(pixmap)
+        cursor = QCursor(Qt.OpenHandCursor)
+        drag.setHotSpot(e.pos() - posInTab)
+        drag.setDragCursor(cursor.pixmap(), Qt.MoveAction)
+        dropAction = drag.exec_(Qt.MoveAction)
+
+    def dragEnterEvent(self, e):
+        e.accept()
+        if e.source().parentWidget() != self:
+            return
+
+        self.parent.TABINDEX = self.indexOf(self.widget(self.indexTab))
+
+    def dragLeaveEvent(self,e):
+        e.accept()
+
+    def dropEvent(self, e):
+        if e.source().parentWidget() == self:
+            return
+
+        e.setDropAction(Qt.MoveAction)
+        e.accept()
+        counter = self.count()
+
+        if counter == 0:
+            self.addTab(e.source().parentWidget().widget(self.parent.TABINDEX), e.source().tabText(self.parent.TABINDEX))
+        else:
+            self.insertTab(counter + 1, e.source().parentWidget().widget(self.parent.TABINDEX),
+                           e.source().tabText(self.parent.TABINDEX))
+
--- main_window.py
+++ main_window.py
@@ -1,49 +1,119 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-from PyQt5 import uic
 import sys
 from PyQt5.QtWebEngineWidgets import *
+from dragable_tabs import DragableTabs
 
 
-class Window(QMainWindow):
+class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.columns_n = 1
+        self.setWindowTitle('Secuted')
+        self.setGeometry(500, 250, 800, 600)
+
+        self.layout = QHBoxLayout()
+        self.setLayout(self.layout)
+
+        self.tab_widget_1 = DragableTabs(self)
+        text_edit = QTextEdit()
+        self.tab_widget_1.addTab(text_edit, 'untitled')
+        self.layout.addWidget(self.tab_widget_1)
+        self.columns_count = 1
+
+        self.menubar = QMenuBar(self)
+        self.init_actions()
+        self.init_menus()
+        self.layout.setMenuBar(self.menubar)
+
+        self.connect_actions()
+
         with open('./css/styles.css') as ss:
             self.styleshit = ss.read()
         self.setStyleSheet(self.styleshit)
-        uic.loadUi('main_window.ui', self)
-        self.actionSet_two_columns_layout.triggered.connect(self.two_columns_layout)
-        self.tabWidget.tabCloseRequested.connect(self.close_tab)
-        self.actionOpen_Brouser.triggered.connect(self.open_browser)
-        self.tab.installEventFilter(self)
+
+    def open_file(self):
+        fname = QFileDialog.getOpenFileName(self, 'Choose a file', '')[0]
+        try:
+            with open(fname) as f:
+                data = f.read()
+                new_text_edit = QTextEdit()
+                new_text_edit.setText(data)
+                self.tab_widget_1.addTab(new_text_edit, fname)
+        except Exception as e:
+            print(e)
+
+    def new_file(self):
+        text_edit = QTextEdit()
+        self.tab_widget_1.addTab(text_edit, 'untitled')
+
+    def save_file_as(self):
+        fname = QFileDialog.getSaveFileName()[0]
+        print(fname)
+        text_edit = self.tab_widget_1.widget(self.tab_widget_1.currentIndex())
+        with open(fname, 'w', encoding='UTF-8') as f:
+            f.write(text_edit.toPlainText())
+
+    def init_actions(self):
+        """Initializes actions"""
+        # File menu actions
+        self.new_action = QAction('New', self)
+        self.open_action = QAction('Open', self)
+        self.save_action = QAction('Save', self)
+        self.save_as_action = QAction('Save as', self)
+        self.close_file_action = QAction('Close file', self)
+
+        # Browser menu actions
+        self.open_browser_action = QAction('Open Browser', self)
+
+        # View menu actions
+        self.two_col_layout_action = QAction('Set two columns layout', self)
+
+    def init_menus(self):
+        """Initializes menus and adds actions"""
+        # File menu
+        self.menu_file = self.menubar.addMenu("File")
+        self.menu_file.addAction(self.new_action)
+        self.menu_file.addAction(self.open_action)
+        self.menu_file.addAction(self.save_action)
+        self.menu_file.addAction(self.save_as_action)
+        self.menu_file.addAction(self.close_file_action)
+
+        # Browser menu
+        self.menu_browser = self.menubar.addMenu("Browser")
+        self.menu_browser.addAction(self.open_browser_action)
+
+        # View menu
+        self.menu_view = self.menubar.addMenu('View')
+        self.menu_view.addAction(self.two_col_layout_action)
+
+    def connect_actions(self):
+        """Connects actions with functions"""
+        # File menu actions
+        self.new_action.triggered.connect(self.new_file)
+        self.open_action.triggered.connect(self.open_file)
+        self.save_as_action.triggered.connect(self.save_file_as)
+
+        # Browser menu actions
+        self.open_browser_action.triggered.connect(self.open_browser)
+
+        # View menu actions
+        self.two_col_layout_action.triggered.connect(self.create_second_column)
+
+    def create_second_column(self):
+        if self.columns_count < 2:
+            self.tab_widget_2 = DragableTabs(self)
+            text_edit = QTextEdit()
+            self.tab_widget_2.addTab(text_edit, 'untitled')
+            self.layout.addWidget(self.tab_widget_2)
+            self.columns_count += 1
 
     def open_browser(self):
-        self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl("http://www.google.com"))
-        self.tabWidget.addTab(self.browser, 'Browser')
-
-    def two_columns_layout(self):
-        if self.columns_n < 2:
-            self.right_column = QTabWidget()
-            tab = QTextEdit()
-            self.right_column.addTab(tab, 'Open file')
-            self.horizontalLayout.addWidget(self.right_column)
-            self.columns_n += 1
-            self.right_column.setTabsClosable(True)
-            self.right_column.setDocumentMode(True)
-            self.right_column.setMovable(True)
-            self.right_column.setTabShape(QTabWidget.Triangular)
-            self.right_column.setStyleSheet(self.styleshit)
-            tab.setStyleSheet(self.styleshit)
-
-    def eventFilter(self, obj, event):
-        if event.type() == QEvent.ContextMenu:
-            menu = QMenu(self)
-            menu.addAction('ad')
-            menu.exec_(event.globalPos())
-            return True
-        return False
+        try:
+            self.browser = QWebEngineView()
+            self.browser.setUrl(QUrl("http://www.google.com"))
+            self.tab_widget_1.addTab(self.browser, 'Browser')
+        except:
+            pass
 
 
 if __name__ == '__main__':
